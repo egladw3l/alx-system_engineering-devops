bef5a80bd80a26439b75b283a37342a9679ad963
egladw3l <gmadziva14@gmail.com>
2023-06-23 03:01:43 -0700

    ab

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,6 @@
-# Create a file using puppet
-
-file { '/tmp/holberton':
+#creates a file
+file {'/tmp/school':
+  ensure  => file,
   path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Install the puppet package
 
 package { 'puppet-lint':
-  ensure   => '2.1.1',
+  ensure   => '2.1.0',
   provider => 'gem',
 }
